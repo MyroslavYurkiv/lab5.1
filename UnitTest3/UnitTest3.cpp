3eed2719524244a30d865f062b07827f076c100c
Yurkivy <myroslav-petro.yurkiv.itis.2020@lpnu.ua>
2020-11-25 23:51:31 +0200

    lab5.1

---
+++ UnitTest3/UnitTest3.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest3
+{
+	TEST_CLASS(UnitTest3)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			double s = {1};
+			t = p(s);
+			Assert::AreEqual(t, 1);
+		}
+	};
+}
